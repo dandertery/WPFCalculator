6abe7b667cf792ea24b72bf29cb322cfd614242c
dandertery <archierowbotham2021@gmail.com>
2023-06-30 17:34:22 +0100

    a

--- WPFCalculator/View/UserControls/Graphing.xaml.cs
+++ WPFCalculator/View/UserControls/Graphing.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 
 namespace WPFCalculator.View.UserControls
@@ -21,21 +23,61 @@ namespace WPFCalculator.View.UserControls
     /// <summary>
     /// Interaction logic for Graphing.xaml
     /// </summary>
-    public partial class Graphing : UserControl
+    public partial class Graphing : UserControl, INotifyPropertyChanged
     {
+        
         public Graphing()
         {
+            DataContext = this;
             InitializeComponent();
+            cartChart.TooltipFindingStrategy = LiveChartsCore.Measure.TooltipFindingStrategy.CompareAllTakeClosest;
+            cartChart.TooltipPosition = LiveChartsCore.Measure.TooltipPosition.Bottom;
+            cartChart.EasingFunction = null;
         }
-
-
-        public ISeries[] Series { get; set; } = new ISeries[]
+        private ISeries[] vals { get; set; } = new ISeries[]
         {
+
+    
                 new LineSeries<double>
                 {
                     Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
-                    Fill = null
+                    Fill = null,  
+                    GeometrySize = 0,
+                    //TooltipLabelFormatter = (chartPoint) => $"({RoundTo(chartPoint.SecondaryValue, 4)}, {RoundTo(chartPoint.PrimaryValue, 4)})"
+
+                },
+
+                new LineSeries<double>
+                {
+                    Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                    Fill = null,
+                    GeometrySize = 0,
+                    //TooltipLabelFormatter = (chartPoint) => $"({RoundTo(chartPoint.SecondaryValue, 4)}, {RoundTo(chartPoint.PrimaryValue, 4)})"
+
                 }
         };
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ISeries[] Vals
+        {
+            get { return vals; }
+            set
+            {
+                vals = value;
+                OnPropertyChanged("Vals");
+
+            }
+        }
+        private double RoundTo(double input, int sf)
+        {
+            return Math.Round(input, sf);
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
